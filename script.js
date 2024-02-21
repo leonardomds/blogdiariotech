61d29c056cfed9c797193a62e14a4f10c2b0bfa1
Leonardo Mateus <mcleorm@hotmail.com>
2024-02-21 16:57:49 -0300

    novas implementações

---
+++ script.js
@@ -0,0 +1,9 @@
+function abrirMenu(){
+    document.getElementById('menu').style.width = '200px';
+    document.getElementById('conteudo').style.marginLeft = '200px';
+}
+
+function fecharMenu(){
+    document.getElementById('menu').style.width = '0px';
+    document.getElementById('conteudo').style.marginLeft = '0px';
+}
